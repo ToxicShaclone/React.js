163bfcfc4a175e17e31862f3a64e9f131413a926
Emiliano Rodriguez <emi.y.shagod@gmail.com>
2021-11-16 23:00:36 -0300

    Clase3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from "react";
+import NavBar from "./components/NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App (){
+    return (
+        <Fragment>
+            <NavBar />
+            <div className="tiendaH1">
+                <h1>
+                    Domando Termis
+                </h1>
+            </div>
+        </Fragment>
+    )
 }
-
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import "./estilos.css"
+
+const  NavBar = () => {
+    return (
+        <nav className="menu">
+            <ul className="menuUl">
+                <li><a href="https://www.google.com/">home</a></li>
+                <li><a href="https://www.google.com/">galeria</a></li>
+                <li><a href="https://www.google.com/">sobre nosotros</a></li>
+                <li><a href="https://www.google.com/">contactos</a></li>
+            </ul>
+        </nav>
+    )
+};
+
+export default NavBar
